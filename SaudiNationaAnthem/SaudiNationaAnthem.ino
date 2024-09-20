2f806b0960b7cec023af6ae8e74d6af6d7e5356b
Fatimah Alabbas <112693194+FatoomRe@users.noreply.github.com>
2024-09-20 10:54:42 +0300

    Add files via upload

---
+++ SaudiNationaAnthem/SaudiNationaAnthem.ino
@@ -0,0 +1,284 @@
+
+void SaudiNationalAnthem(int buzzerPin) {
+
+  tone(buzzerPin, 349);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(1200);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(1200);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 698);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 659);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(600);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 659);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 698);
+  delay(1200);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(1500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(600);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 587);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(300);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(600);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 698);
+  delay(600);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(500);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 523);
+  delay(125);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 698);
+  delay(600);
+  noTone(buzzerPin);
+}
+
+void setup() {
+
+  SaudiNationalAnthem(8);
+}
+
+void loop() {
+  // No need for loop code
+}
